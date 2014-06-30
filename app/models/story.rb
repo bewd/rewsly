e37bd8a198dd16905d5aa6f03807dedff75ed27a
jleo3 <joseph.leo3@gmail.com>
2014-06-30 19:22:53 -0400

    a story belongs_to a user

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,7 +1,10 @@
 class Story < ActiveRecord::Base
   validates :title, :link, :category, :upvotes, presence: true
+
   scope :popular, -> { where('upvotes >= ?', 4)}
   scope :recent, -> { where('created_at >= ?', Date.today)}
+
+  belongs_to :user
 
   def self.search_for(query)
     where('title LIKE :query OR category LIKE :query', query: "%#{query}%")
